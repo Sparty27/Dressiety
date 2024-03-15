f10c419b5ce73b65cbfd6ac4a374029a4205e754
Sparty27 <nyshchyi.nazar@gmail.com>
2024-03-15 16:41:50 +0200

    Add migrations for Products, Photos, Tags, and Product-Tag pivot

--- database/migrations/2024_03_10_161200_create_photos_table.php
+++ database/migrations/2024_03_10_161200_create_photos_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->string('name');
-            $table->boolean('status');
+            $table->morphs('photoble');
+            $table->string('url');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('photos');
     }
 };
--- database/migrations/2024_03_10_173621_create_tags_table.php
+++ database/migrations/2024_03_10_173621_create_tags_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->boolean('status');
             $table->timestamps();
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('tags');
     }
 };
